6c35ee21bbb1ddd25c1d429cc754c01ca1a1a989
Ticklol <robertoschenkd@gmail.com>
2020-01-31 23:53:13 -0300

    Movement Example

---
+++ Assets/Scripts/CharacterMovementComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovementComponent : MovementComponent
+{
+    public float Speed;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += MovementInput.normalized * Speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public MovementComponent moveComponent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveComponent = GetComponent<MovementComponent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveComponent.MovementInput = new Vector3(0,0,0);
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            moveComponent.MovementInput = new Vector3(0,1,0);
+        }
+    }
+}

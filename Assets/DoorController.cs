f586bfabd056f8e5d5fccdbe16dcd5e83d331f3c
Pedro Grossmann <pedro.grossmann3@gmail.com>
2020-02-01 07:31:53 -0300

    Portinhas e sistema de eventos
    pq sim

---
+++ Assets/DoorController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+
+    public GameObject door;
+    public int id;
+
+    void Start()
+    {
+        GameEvents.current.onDoorwayTriggerEnter += OnDoorwayOpen;
+        GameEvents.current.onDoorwayTriggerExit += OnDoorwayClose;
+    }
+
+    private void OnDoorwayClose(int id)
+    {
+        if (id != this.id)
+        {
+            return;
+        }
+        door.SetActive(true); //Trocar isso por fazer girar de volta e colocar a colisão
+    }
+
+    private void OnDoorwayOpen(int id)
+    {
+        if (id != this.id)
+        {
+            return;
+        }
+        door.SetActive(false);//trocar isso por tirar a colisao e fazer girar
+    }
+
+
+}

7bc7017b3dbda8198959e9a13460e7197e7e2d3d
Pedro Grossmann <pedro.grossmann3@gmail.com>
2020-02-02 00:50:32 -0300

    menu update

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,25 +7,30 @@ public class MainMenu : MonoBehaviour
 {
     public GameObject optionsPanel;
     public GameObject creditsPanel;
+    public GameObject menuPanel;
 
     public void OpenOptions()
     {
         optionsPanel.SetActive(true);
+        menuPanel.SetActive(false);
     }
 
     public void CloseOptions()
     {
         optionsPanel.SetActive(false);
+        menuPanel.SetActive(true);
     }
 
     public void OpenCredits()
     {
         creditsPanel.SetActive(true);
+        menuPanel.SetActive(false);
     }
 
     public void CloseCredits()
     {
         creditsPanel.SetActive(false);
+        menuPanel.SetActive(true);
     }
 
     public void ChangeScene(int id)
